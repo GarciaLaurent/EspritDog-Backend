49492ec1b0009040fcc84a08b997160c15f9b45a
Kieu Anh Tuan <anhtuan.kieu@maiia.com>
2021-04-01 14:34:04 +0200

    Add photo

--- packages/admin/src/orders/OrderList.tsx
+++ packages/admin/src/orders/OrderList.tsx
@@ -1,5 +1,43 @@
 import * as React from 'react';
-import { List, Datagrid, TextField, EmailField, ListProps } from 'react-admin';
+import {
+  List,
+  Datagrid,
+  TextField,
+  EmailField,
+  ListProps,
+  FieldProps,
+} from 'react-admin';
+import { get, isEmpty } from 'lodash';
+import MUIList from '@material-ui/core/List';
+import MUIListItem from '@material-ui/core/ListItem';
+import MUIListItemText from '@material-ui/core/ListItemText';
+
+const PhotoField: React.FC<FieldProps> = ({ record, source }) => {
+  if (!record || !source || isEmpty(get(record, source))) {
+    return null;
+  }
+  return <img src={get(record, source)} />;
+};
+
+const ItemsField: React.FC<FieldProps> = ({ record, source }) => {
+  if (!record || !source || isEmpty(get(record, source))) {
+    return null;
+  }
+  const items = get(record, source);
+  return (
+    <MUIList dense={true}>
+      {items.map((item: any) => {
+        return (
+          <MUIListItem>
+            <MUIListItemText>
+              {item.name} : {item.quantity}
+            </MUIListItemText>
+          </MUIListItem>
+        );
+      })}
+    </MUIList>
+  );
+};
 
 const UserList: React.FC<ListProps> = (props) => (
   <List {...props}>
@@ -7,6 +45,8 @@ const UserList: React.FC<ListProps> = (props) => (
       <TextField source="id" />
       <TextField source="user" />
       <TextField source="pharmacy" />
+      <ItemsField source="items" />
+      <PhotoField source="photo" />
       <TextField source="status" />
     </Datagrid>
   </List>
